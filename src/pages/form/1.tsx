f6f386894c636a355693b2231b462812b0bf077a
WeeranutCK <weeranut.c@ku.th>
2023-11-29 08:00:45 +0700

    Finish form module

--- src/pages/form/1.tsx
+++ src/pages/form/1.tsx
@@ -10,16 +10,17 @@ const _1: NextPage<Props> = () => {
         <AntLayout>
             <Form className="m-5 grid">
                 <FormGenerator type={"text"} title={"ชื่อองค์กรกิจกรรมนิสิต"} />
+                <FormGenerator type={"text"} title={"ที่ออกหนังสือ"} />
+                <FormGenerator type={"date"} title={"วัน เดือน ปี (ที่ออกหนังสือ)"} />
                 <FormGenerator type={"text"} title={"โครงการ"} />
+                <FormGenerator type={"date"} title={"วัน เดือน ปี (ที่จัดโครงการ)"} />
                 <FormGenerator type={"text"} title={"สถานที่จัดโครงการ"} />
-                <FormGenerator type={"date"} title={"วัน เดือน ปี"} />
-                <FormGenerator type={"number"} title={"งบประมาณ"} placeholder={"จำนวนเงิน (บาท)"} />
-                <FormGenerator type={"number"} title={"จำนวนเงินเหลือเพื่อส่งคืน"} placeholder={"จำนวนเงิน (บาท)"} />
-                <FormGenerator type={"text"} title={"ปัญหาและอุปสรรค"} />
-                <FormGenerator type={"text"} title={"ข้อเสนอแนะ"} />
-                <FormGenerator type={"text"} title={"ผู้ยืมเงิน"} placeholder={"ประธานโครงการ"} />
+                <FormGenerator type={"number"} title={"จำนวนเงิน"} placeholder={"จำนวนเงิน (บาท)"} />
+                <FormGenerator type={"text"} title={"เหรัญญิก องค์กร"} />
                 <FormGenerator type={"text"} title={"ประธานองค์กร"} />
                 <FormGenerator type={"text"} title={"อาจารย์ที่ปรึกษาองค์กร"} />
+                <FormGenerator type={"text"} title={"ผู้ยืมเงิน"} />
+                <FormGenerator type={"upload"} title={"Upload"} file_type={".pdf"} />
             </Form>
         </AntLayout>
     )
--- src/utils/form_generator.tsx
+++ src/utils/form_generator.tsx
@@ -1,10 +1,13 @@
-import {Form, Input} from "antd";
+import { Form, Input, Upload, message, Button } from "antd";
+import { UploadOutlined } from '@ant-design/icons';
+import type { UploadProps } from 'antd';
 
 type FormInputAttributes = {
     type: string;
     title: string;
     placeholder?: string;
     regex?: string;
+    file_type?: string;
 }
 
 export default function FormGenerator(attributes: FormInputAttributes) {
@@ -13,14 +16,39 @@ export default function FormGenerator(attributes: FormInputAttributes) {
             <div>{title}</div>
             <Input placeholder={placeholder ? placeholder : title} type={type} pattern={regex ? regex : ""} />
         </Form.Item>
-    );
+    )
 
-    const FormDateInput = ({ title }: { title: string; }) => (
+    const FormDateInput = ({ type, title }: { type: string; title: string; }) => (
         <Form.Item className="flex flex-col my-2 w-1/4">
             <div>{title}</div>
-            <Input type="date" />
+            <Input type={type} />
         </Form.Item>
-    );
+    )
+
+    const FormImageUpload = ({ title, file_type }: { title: string; file_type: string }) => {
+        const props: UploadProps = {
+            beforeUpload: (file) => {
+                const isValid = file.type === file_type;
+                if (!isValid) {
+                    message.error(`${file.name} is not a ${file_type} file`);
+                }
+                return isValid || Upload.LIST_IGNORE;
+            },
+            onChange: (info) => {
+                console.log(info.fileList);
+            },
+            accept: file_type,
+        };
+
+        return (
+            <Form.Item className="flex my-2 w-1/4">
+                <div>{title}</div>
+                <Upload {...props}>
+                    <Button icon={<UploadOutlined />}>Click to Upload</Button>
+                </Upload>
+            </Form.Item>
+        );
+    };
 
     switch (attributes.type) {
         case "text":
@@ -33,7 +61,13 @@ export default function FormGenerator(attributes: FormInputAttributes) {
             />
         case "date":
             return <FormDateInput
+                type={attributes.type}
                 title={attributes.title}
+            />
+        case "upload":
+            return <FormImageUpload
+                title={attributes.title}
+                file_type={attributes.file_type!}
             />
     }
 }
