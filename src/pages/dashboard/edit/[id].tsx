c2915c3b9e8404e3cea0e8f670f279518bc5ed6b
Teerut Srithongdee <teerutsr@gmail.com>
2024-01-27 19:36:30 +0700

    Refactor variable names for clarity

---
+++ src/pages/dashboard/edit/[id].tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+
+interface Props {}
+
+const EditProject: NextPage<Props> = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default EditProject
