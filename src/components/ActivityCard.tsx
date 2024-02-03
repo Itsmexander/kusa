9a79cc10e5ce8ce74c20755d6d5196dea13f7d31
xeff09 <wongchaita03@gmail.com>
2024-02-04 03:02:17 +0700

    fix: roughly fix text-overflow

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -1,25 +1,42 @@
-import FakeActivityInterface from "public/home-fakes/FakeActivityInterface"
+import FakeActivityInterface from "public/home-fakes/FakeActivityInterface";
 
-const thaiMonthNames = ['มค', 'กพ', 'มีค', 'เมย', 'พค', 'มิย', 'กค', 'สค', 'กย', 'ตค', 'พย', 'ธค'];
+const thaiMonthNames = [
+  "มค",
+  "กพ",
+  "มีค",
+  "เมย",
+  "พค",
+  "มิย",
+  "กค",
+  "สค",
+  "กย",
+  "ตค",
+  "พย",
+  "ธค",
+];
 
 const ActivityCard = (details: FakeActivityInterface) => {
+  const s_month_name = thaiMonthNames[details.range.start.getMonth()];
+  const e_month_name = thaiMonthNames[details.range.end.getMonth()];
 
-    const s_month_name = thaiMonthNames[details.range.start.getMonth()];
-    const e_month_name = thaiMonthNames[details.range.end.getMonth()];
+  return (
+    <div className="flex h-32 w-full flex-row rounded-md border-2 border-primary">
+      <div className="flex h-full w-2/5 flex-col items-center justify-center overflow-hidden rounded-md bg-primary text-white">
+        <p className="text-4xl font-bold">
+          {details.range.start.getDate()}-{details.range.end.getDate()}
+        </p>
+        <p className="text-lg">
+          {s_month_name == e_month_name
+            ? s_month_name
+            : s_month_name + "-" + e_month_name}
+        </p>
+      </div>
+      <div className="flex h-full w-full flex-col items-start justify-center overflow-hidden rounded-md px-3 text-black">
+        <p className="text-3xl font-bold">{details.name.th}</p>
+        <p className="text-lg">{details.name.en}</p>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="flex flex-row border-2 border-primary h-32 w-full rounded-md">
-            <div className="flex flex-col h-full w-2/5 bg-primary rounded-md justify-center items-center text-white">
-                <p className="text-4xl font-bold">{details.range.start.getDate()}-{details.range.end.getDate()}</p>
-                <p className="text-lg">{s_month_name == e_month_name? s_month_name: s_month_name + '-' + e_month_name}</p>
-            </div>
-            <div className="flex flex-col h-full w-full rounded-md justify-center items-start text-black px-3">
-                <p className="text-3xl font-bold">{details.name.th}</p>
-                <p className="text-lg">{details.name.en}</p>
-            </div>
-        </div>
-    )
-    
-}
-
-export default ActivityCard
+export default ActivityCard;
--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Avatar, Button, List, message } from 'antd';
-import VirtualList from 'rc-virtual-list';
-import FakeActivities from 'public/home-fakes/FakeActivities';
-import FakeActivityInterface from 'public/home-fakes/FakeActivityInterface';
-import ActivityCard from './ActivityCard';
+import React, { useEffect, useState } from "react";
+import { Avatar, Button, List, message } from "antd";
+import VirtualList from "rc-virtual-list";
+import FakeActivities from "public/home-fakes/FakeActivities";
+import FakeActivityInterface from "public/home-fakes/FakeActivityInterface";
+import ActivityCard from "./ActivityCard";
 
-const ContainerHeight = 450;
+const ContainerHeight = 500;
 const ItemsPerLoad = 3;
 
 const App = () => {
@@ -25,21 +25,27 @@ const App = () => {
   }, []);
 
   const onScroll = (e: React.UIEvent<HTMLElement, UIEvent>) => {
-    if (Math.abs(e.currentTarget.scrollHeight - e.currentTarget.scrollTop - ContainerHeight) <= 1) {
+    if (
+      Math.abs(
+        e.currentTarget.scrollHeight -
+          e.currentTarget.scrollTop -
+          ContainerHeight,
+      ) <= 1
+    ) {
       appendData();
     }
   };
 
   const onChangeExpansion = () => {
     setExpansion(!expansion);
-  }
+  };
 
   return (
     <>
       <List>
         <VirtualList
           data={data}
-          height={expansion? 0:ContainerHeight}
+          height={expansion ? 0 : ContainerHeight}
           itemHeight={47}
           itemKey="id"
           onScroll={onScroll}
@@ -50,25 +56,26 @@ const App = () => {
                 id={item.id}
                 range={{
                   start: item.range.start,
-                  end: item.range.end
+                  end: item.range.end,
                 }}
                 name={{
                   en: item.name.en,
-                  th: item.name.th
+                  th: item.name.th,
                 }}
               />
             </List.Item>
           )}
         </VirtualList>
       </List>
-      <div className="w-full flex justify-center items-center p-5">
-          <Button 
-              type="primary" 
-              shape="round" 
-              className="w-2/6" 
-              onClick={onChangeExpansion}
-          >ทั้งหมด
-          </Button>
+      <div className="flex w-full items-center justify-center p-5">
+        <Button
+          type="primary"
+          shape="round"
+          className="w-2/6"
+          onClick={onChangeExpansion}
+        >
+          ทั้งหมด
+        </Button>
       </div>
     </>
   );
