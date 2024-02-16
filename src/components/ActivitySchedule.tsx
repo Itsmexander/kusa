0f858edd908f1394537b0786aac490064422412b
Teerut Srithongdee <teerutsr@gmail.com>
2024-02-16 17:24:38 +0700

    Refactor component styles and layout

--- src/components/ActivitySchedule.tsx
+++ src/components/ActivitySchedule.tsx
@@ -4,7 +4,7 @@ const ActivitySchedule = () => {
 
 
     return (
-        <div className="bg-white w-full 2xl:w-5/12 h-full rounded-md flex flex-col p-5 gap-5">
+        <div className="bg-white w-full h-full rounded-md flex flex-col p-5 gap-5">
             <div className="h-24 w-full flex flex-col justify-center items-center text-black">
                 <div className="text-3xl font-bold text-primary">ปฏิทินกิจกรรม</div>
                 <div className="border-b-4 border-b-secondary w-8"></div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,14 +7,17 @@ export default function Home() {
 
     return (
         <AntLayout>
-            <div className="flex flex-col px-28 p-10 gap-10 h-full">
+            <div className="flex flex-col gap-3 h-full">
                 <div className="w-full h-fit">
                     <Carousel />
                 </div>
-                <div className="flex flex-col 2xl:flex-row min-h-[45rem] h-full w-full gap-5 ">
+                <div className="grid grid-cols-3 gap-5">
                     <ActivitySchedule />
                     <Announcement />
                 </div>
+                {/* <div className="flex flex-col md:flex-row min-h-[45rem] h-full w-full gap-5 ">
+                    
+                </div> */}
             </div>
         </AntLayout>
     );
