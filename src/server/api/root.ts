7502b97ae7b788743fce825bc8069a923d045dbb
Teerut26 <teerutsr@gmail.com>
2024-02-23 20:41:12 +0700

    Add new routers for CLUB, SAB, SC, and SD

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,8 @@
-import { exampleRouter } from "@/server/api/routers/example";
 import { createTRPCRouter } from "@/server/api/trpc";
+import { CLUBRouter } from "./routers/CLUB";
+import { SABRouter } from "./routers/SAB";
+import { SCRouter } from "./routers/SC";
+import { SDRouter } from "./routers/SD";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +10,10 @@ import { createTRPCRouter } from "@/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  club: CLUBRouter,
+  sab: SABRouter,
+  sc: SCRouter,
+  sd: SDRouter,
 });
 
 // export type definition of API
