dc91761faca1cb7ef6da0d8aa8038c40f0251d55
Teerut26 <teerutsr@gmail.com>
2024-02-23 20:16:40 +0700

    Refactor code to improve performance and readability

---
+++ src/pages/notice.tsx
@@ -0,0 +1,12 @@
+import AntLayout from '@/layouts/AntLayout'
+import React from 'react'
+
+export default function notice() {
+    return (
+        <AntLayout>
+            <div className="flex flex-col gap-3 h-full">
+                notice
+            </div>
+        </AntLayout>
+    )
+}
